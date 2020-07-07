9cc92d8593b9e24cb9d3e9941348326159087de6
megan <meggsnbacon777@gmail.com>
2020-07-07 09:25:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,6 +48,8 @@ def monopoly_with_fourth_tier =
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
     },
+    :names=>
+    {
       :reading_railroad=>
       {
         "mortgage_value"=>"$100"
