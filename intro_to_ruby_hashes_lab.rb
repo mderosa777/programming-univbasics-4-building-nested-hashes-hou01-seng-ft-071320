15542529a61ca1df53e4f8b14cf57f7e55894c0c
megan <meggsnbacon777@gmail.com>
2020-07-07 08:12:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ def base_hash => {}
 
 end
 
-def monopoly_with_second_tier=>{
+def monopoly_with_second_tier=>{def base_hash => {}
 }
 
   # When you start writing the implementation for this method, copy the Hash
