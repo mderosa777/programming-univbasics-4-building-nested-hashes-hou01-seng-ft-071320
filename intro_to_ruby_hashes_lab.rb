7e0f106f764e5f175a1a3ca1a08a59a338e78edc
megan <meggsnbacon777@gmail.com>
2020-07-07 09:24:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,15 +36,18 @@ def monopoly_with_third_tier
 
 end
 
-def monopoly_with_fourth_tier 
- {:railroads =>
- {:pieces => 4,:rent_in_dollars =>
- {:one_piece_owned => 25,
- :two_pieces_owned => 50,
- :three_pieces_owned => 100,
- :four_piece_owned => 200},
- :names=>
+def monopoly_with_fourth_tier =
+ {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
     {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
       :reading_railroad=>
       {
         "mortgage_value"=>"$100"
