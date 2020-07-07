c34c1c5260dca25da66837ced65f0693af40b82e
megan <meggsnbacon777@gmail.com>
2020-07-07 09:12:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,7 +56,7 @@ def monopoly_with_fourth_tier
       :b_and_o_railroad=>
       {
         "mortgage_value"=>"$400"
-      },
+      }
       :shortline=>
       {
         "mortgage_value"=>"$800"
