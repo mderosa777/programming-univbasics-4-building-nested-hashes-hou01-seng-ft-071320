eef8932363038cce532d6f6f632e679e8c00f93b
megan <meggsnbacon777@gmail.com>
2020-07-07 08:18:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,6 @@
-def base_hash => {} 
+def base_hash => {
+  
+ 
  
   
   # Remember implicit return! We're returning this new Hash _without_ the
@@ -7,7 +9,7 @@ def base_hash => {}
 
 
 def monopoly_with_second_tier=>{
-  def base_hash => {}
+  
   
 }
 
@@ -18,8 +20,9 @@ def monopoly_with_second_tier=>{
 
 end
 
-def monopoly_with_third_tier=>{ def monopoly_with_second_tier=>{def base_hash => {}
-}}
+def monopoly_with_third_tier=>{ 
+  
+}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
@@ -27,9 +30,7 @@ def monopoly_with_third_tier=>{ def monopoly_with_second_tier=>{def base_hash =>
 
 end
 
-def monopoly_with_fourth_tier => {def monopoly_with_third_tier=>{ def monopoly_with_second_tier=>{def base_hash => {}
-}}
-  
+def monopoly_with_fourth_tier => {
 }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
